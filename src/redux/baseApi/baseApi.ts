28dfe0b57b96d15a2cf6e530da5875f06b130ace
mohabbatali <mohabbatali@iciclecorporation.com>
2025-08-17 17:36:45 +0600

    redux configure, seperate sidebar and header

--- src/redux/baseApi/baseApi.ts
+++ src/redux/baseApi/baseApi.ts
@@ -3,8 +3,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:4000/api/v1",
-    // baseUrl: "https://molla-departmental-server-new.vercel.app/api/v1",
+    // baseUrl: "http://localhost:4000/api/v1",
+    baseUrl: "https://sazu-khadem-backend.vercel.app/api/v1",
   }),
   tagTypes: ["Portfolio"],
   endpoints: () => ({}),
